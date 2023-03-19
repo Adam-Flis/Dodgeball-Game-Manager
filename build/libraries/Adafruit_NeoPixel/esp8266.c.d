2981d7aa65626993106909edc7c8fd552d253494
Adam F <59700364+Adam-Flis@users.noreply.github.com>
2023-03-19 15:10:09 -0400

    Revert "Removed .gitignore"
    This reverts commit 50bfceaf903eb8693e32d779f70f59c0d10bda1d.

--- build/libraries/Adafruit_NeoPixel/esp8266.c.d
+++
@@ -1,2 +0,0 @@
-C:\Users\adamf_6sjoanp\Documents\Coding\NCDA-Shotclock\build\libraries\Adafruit_NeoPixel\esp8266.c.o: \
- C:\Users\adamf_6sjoanp\Documents\Arduino\libraries\Adafruit_NeoPixel\esp8266.c
