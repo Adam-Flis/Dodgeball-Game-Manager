c41aae6a640065833a5269ac39d0925ff57e1786
Nathan Zarif <nathanzarif@gmail.com>
2023-03-20 21:27:23 -0400

    commit message

---
+++ web/Routes/ShotClockPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ShotClockPage() {
+    return (
+        <div className="ShotClockPage">
+            <h1>Shot Clock Page</h1>
+        </div>
+    );
+}
+
+export default ShotClockPage;
